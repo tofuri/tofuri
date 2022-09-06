5993bffefb322af92402148f9115f8c4e93800b8
Lyserith <Lyserith@pm.me>
2022-09-06 19:53:47 +0200

    improved efficiency

--- examples/amount.rs
+++ examples/amount.rs
@@ -1,6 +1,6 @@
 use pea::amount::{from_bytes, to_bytes};
 fn main() {
-    let a = 0xff000000000000000000000000000000;
+    let a = 0xff0000000000000000000000000000;
     println!("{}", a);
     let b = to_bytes(a);
     println!("{:x?}", b);
--- src/amount.rs
+++ src/amount.rs
@@ -10,22 +10,26 @@ pub fn to_bytes(input: u128) -> [u8; BYTES] {
     }
     let size = 16 - i;
     let mut output = [0; BYTES];
-    output[BYTES - 1] = size as u8;
-    for j in 0..BYTES - 1 {
+    for j in 0..BYTES {
         let k = i + j;
         if k == 16 {
             break;
         }
         output[j] = bytes[k];
     }
+    output[BYTES - 1] = (output[BYTES - 1] & 0xf0) | size as u8;
     output
 }
 pub fn from_bytes(input: [u8; BYTES]) -> u128 {
-    let size = input[BYTES - 1] as usize;
+    let size = input[BYTES - 1] as usize & 0x0f;
     let mut bytes = [0; 16];
-    for i in 0..BYTES - 1 {
+    for i in 0..BYTES {
         let j = 16 - size + i;
         if j == 16 {
+            break;
+        }
+        if i == BYTES - 1 {
+            bytes[j] = input[i] & 0xf0;
             break;
         }
         bytes[j] = input[i];
@@ -39,11 +43,15 @@ mod tests {
     use test::Bencher;
     #[test]
     fn test_encode() {
-        assert_eq!(vec![1, 0, 0, 9], to_bytes(0x10000000000000000));
+        assert_eq!([1, 0, 0, 9], to_bytes(0x10000000000000000));
     }
     #[test]
     fn test_decode() {
         assert_eq!(0x10000000000000000, from_bytes([1, 0, 0, 9]));
+    }
+    #[test]
+    fn test_decode_size_0xf0() {
+        assert_eq!(0, from_bytes([0xff, 0xff, 0xff, 0xf0]));
     }
     #[bench]
     fn bench_encode(b: &mut Bencher) {

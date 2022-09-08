3fb3466ece5b62017561614d057a126c1090af44
Lyserith <Lyserith@pm.me>
2022-09-08 13:33:14 +0200

    100 transactions per second

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,9 @@
 use crate::types;
 pub const GENESIS_TIMESTAMP: types::Timestamp = 1640995200;
-pub const BLOCK_TRANSACTIONS_LIMIT: usize = 2;
-pub const BLOCK_STAKES_LIMIT: usize = 2;
-pub const PENDING_TRANSACTIONS_LIMIT: usize = 2;
-pub const PENDING_STAKES_LIMIT: usize = 2;
+pub const BLOCK_TRANSACTIONS_LIMIT: usize = BLOCK_TIME_MIN * 100;
+pub const BLOCK_STAKES_LIMIT: usize = BLOCK_TIME_MIN;
+pub const PENDING_TRANSACTIONS_LIMIT: usize = BLOCK_TRANSACTIONS_LIMIT;
+pub const PENDING_STAKES_LIMIT: usize = BLOCK_STAKES_LIMIT;
 pub const PREFIX_ADDRESS: &str = "0x";
 pub const PREFIX_ADDRESS_KEY: &str = "Key0x";
 pub const PROTOCOL_VERSION: &str = "experimental/1.0.0";

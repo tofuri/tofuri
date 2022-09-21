fc41b851bc6194e0f7a8e8ba2ce5dd304b6bf08a
Lyserith <Lyserith@pm.me>
2022-09-21 14:25:35 +0200

    extend branch, new branch, height, insert, fmt

---
+++ examples/tree.rs
@@ -0,0 +1,12 @@
+use pea::tree::Tree;
+fn main() {
+    let mut tree = Tree::new();
+    tree.insert([0x11; 32], [0x00; 32]);
+    tree.insert([0x22; 32], [0x11; 32]);
+    tree.insert([0x33; 32], [0x22; 32]);
+    tree.insert([0x44; 32], [0x33; 32]);
+    tree.insert([0x55; 32], [0x22; 32]);
+    tree.insert([0x66; 32], [0x00; 32]);
+    println!("{:x?}", tree);
+    println!("{:x?}", tree.main());
+}
--- src/tree.rs
+++ src/tree.rs
@@ -1,8 +1,36 @@
-use std::collections::HashMap;
 use crate::types;
+use std::collections::HashMap;
+use std::fmt;
+type Branch = (types::Hash, types::Height);
 pub struct Tree {
-    branches: Vec<(types::Hash, types::Amount)>, // <(leaf, stake sum)>
-    hashes: HashMap<types::Hash, types::Hash>, // <hash, previous hash>
+    branches: Vec<Branch>,
+    hashes: HashMap<types::Hash, types::Hash>,
+}
+impl fmt::Debug for Tree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        #![allow(dead_code)]
+        #[derive(Debug)]
+        struct Tree {
+            branches: Vec<(String, types::Height)>,
+            hashes: HashMap<String, String>,
+        }
+        write!(
+            f,
+            "{:?}",
+            Tree {
+                branches: self
+                    .branches
+                    .iter()
+                    .map(|(hash, height)| (hex::encode(hash), *height))
+                    .collect(),
+                hashes: self
+                    .hashes
+                    .iter()
+                    .map(|(hash, previous_hash)| (hex::encode(hash), hex::encode(previous_hash)))
+                    .collect(),
+            }
+        )
+    }
 }
 impl Tree {
     pub fn new() -> Tree {
@@ -14,15 +42,43 @@ impl Tree {
     fn sort(&mut self) {
         self.branches.sort_by(|a, b| b.1.cmp(&a.1));
     }
-    pub fn main(&mut self) -> Option<types::Hash> {
+    pub fn main(&mut self) -> Option<&Branch> {
         self.sort();
-        match self.branches.first() {
-            Some(branch) => Some(branch.0),
-            None => None,
+        self.branches.first()
+    }
+    pub fn insert(&mut self, hash: types::Hash, previous_hash: types::Hash) {
+        if self.hashes.insert(hash, previous_hash).is_some() {
+            return;
+        }
+        if let Some(index) = self
+            .branches
+            .iter()
+            .position(|(hash, _)| hash == &previous_hash)
+        {
+            // extend branch
+            self.branches[index] = (hash, self.height(previous_hash));
+        } else {
+            // new branch
+            self.branches.push((hash, self.height(previous_hash)));
         }
     }
-    // pub fn main(&mut self) -> Option<&([u8; 32], u128)> {
-    // self.sort();
-    // self.branches.first()
-    // }
+    fn height(&self, previous_hash: types::Hash) -> types::Height {
+        let mut hash = previous_hash;
+        let mut height = 0;
+        loop {
+            match self.hashes.get(&hash) {
+                Some(previous_hash) => {
+                    hash = *previous_hash;
+                    height += 1;
+                }
+                None => {
+                    if hash != [0; 32] {
+                        panic!("broken chain")
+                    }
+                    break;
+                }
+            };
+        }
+        height
+    }
 }

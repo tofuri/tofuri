456d8e2f78bbd3b15178eb35aaef31ea11b99c4a
Lyserith <Lyserith@pm.me>
2022-10-10 21:28:07 +0200

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,6 @@ pub use pea_address as address;
 pub use pea_amount as amount;
 pub use pea_api as api;
 pub use pea_core::{block, constants, stake, transaction, types, util};
+pub use pea_node::{blockchain, db, gossipsub, heartbeat, http, p2p, state, states, sync};
 pub use pea_tree as tree;
 pub use pea_wallet as wallet;

b30c6258d08925ea0b13cc58961cace7d5ce9fd3
Lyserith <Lyserith@pm.me>
2022-11-10 16:38:54 +0100

    charges

--- pay/examples/charges.rs
+++ pay/examples/charges.rs
@@ -2,8 +2,7 @@ use pea_pay::processor::Payment;
 use std::error::Error;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let amount = 100;
-    let charge = reqwest::get(format!("http://localhost:9331/charge/new/{}", amount)).await?.json::<Payment>().await?;
+    let charge = reqwest::get("http://localhost:9331/charges").await?.json::<Payment>().await?;
     println!("{:?}", charge);
     Ok(())
 }

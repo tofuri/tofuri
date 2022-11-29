f676b7ca04daece308aaa54637e924eb69e9d893
Lyserith <Lyserith@pm.me>
2022-11-29 19:00:07 +0100

    sync avg

--- pea/src/sync.rs
+++ pea/src/sync.rs
@@ -2,13 +2,16 @@
 pub struct Sync {
     pub index_0: usize,
     pub index_1: usize,
+    pub avg: f32,
     pub new: usize,
     pub syncing: bool,
 }
 impl Sync {
     pub fn handler(&mut self) {
-        self.syncing = self.new > 1;
+        self.avg += self.new as f32;
+        self.avg /= 2_f32;
         self.new = 0;
+        self.syncing = self.avg > 1_f32;
     }
 }
 impl Default for Sync {
@@ -16,6 +19,7 @@ impl Default for Sync {
         Self {
             index_0: 0,
             index_1: 0,
+            avg: 0.0,
             new: 0,
             syncing: false,
         }

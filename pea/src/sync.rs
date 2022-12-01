4c28d6c8732cdbf477411a1fe2cc7a8dfcbca9d6
Lyserith <Lyserith@pm.me>
2022-12-01 15:50:14 +0100

    bugfix

--- pea/src/sync.rs
+++ pea/src/sync.rs
@@ -1,4 +1,8 @@
+use lazy_static::lazy_static;
 use pea_core::constants::BLOCK_TIME_MIN;
+lazy_static! {
+    static ref BPS: f32 = 0.5_f32 + (1_f32 / 2_f32.powf(BLOCK_TIME_MIN as f32));
+}
 #[derive(Debug)]
 pub struct Sync {
     pub index: usize,
@@ -14,7 +18,7 @@ impl Sync {
         self.completed = !self.downloading();
     }
     pub fn downloading(&self) -> bool {
-        self.bps > 1_f32 / BLOCK_TIME_MIN as f32
+        self.bps > *BPS
     }
 }
 impl Default for Sync {

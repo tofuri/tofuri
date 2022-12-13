7ee5c7c6c6d327e947ad32e01a1f063b7f29a5fa
Lyserith <Lyserith@pm.me>
2022-12-13 21:53:45 +0100

    updates

--- api/examples/api_get_height.rs
+++
@@ -1,8 +0,0 @@
-use pea_api::get;
-use std::error::Error;
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let height = get::height("http://localhost:8080").await?;
-    println!("{}", height);
-    Ok(())
-}

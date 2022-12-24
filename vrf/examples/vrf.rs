03f8e7b2baebcb6999f77901d67d6874da06b88b
Lyserith <Lyserith@pm.me>
2022-12-24 20:54:11 +0100

    sha2

--- vrf/examples/vrf.rs
+++ vrf/examples/vrf.rs
@@ -1,18 +1,18 @@
 use pea_key::Key;
 use pea_vrf::{prove, verify};
-use sha3::Sha3_256;
-use sha3::Sha3_512;
+use sha2::Sha256;
+use sha2::Sha512;
 fn main() {
     let key = Key::generate();
     let mut alpha = [0; 32];
     for _ in 0..3 {
         for _ in 0..2 {
-            let (beta, _) = prove::<Sha3_512, Sha3_256>(&alpha, &key.scalar);
+            let (beta, _) = prove::<Sha512, Sha256>(&alpha, &key.scalar);
             println!("{}", hex::encode(beta));
         }
-        let (beta, pi) = prove::<Sha3_512, Sha3_256>(&alpha, &key.scalar);
+        let (beta, pi) = prove::<Sha512, Sha256>(&alpha, &key.scalar);
         println!("{}", hex::encode(beta));
-        println!("{}", verify::<Sha3_512, Sha3_256>(&alpha, &beta, key.ristretto_point(), &pi));
+        println!("{}", verify::<Sha512, Sha256>(&alpha, &beta, key.ristretto_point(), &pi));
         alpha = beta;
     }
 }

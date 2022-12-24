9caa16aeabb4c19f9ab2c55a7bf355af14a3e08b
Lyserith <Lyserith@pm.me>
2022-12-24 03:40:01 +0100

    updates

--- vrf/examples/vrf.rs
+++ vrf/examples/vrf.rs
@@ -4,8 +4,10 @@ fn main() {
     let key = Key::generate();
     let mut alpha = [0; 32];
     for _ in 0..3 {
-        let (beta, _) = prove(&alpha, &key.scalar);
-        println!("{}", hex::encode(beta));
+        for _ in 0..2 {
+            let (beta, _) = prove(&alpha, &key.scalar);
+            println!("{}", hex::encode(beta));
+        }
         let (beta, pi) = prove(&alpha, &key.scalar);
         println!("{}", hex::encode(beta));
         println!("{}", verify(&alpha, beta, key.ristretto_point(), &pi));

9207167f3dd55805b8c3347eceb02ff7cfb18d59
Lyserith <Lyserith@pm.me>
2022-12-24 20:19:31 +0100

    digest

--- vrf/src/lib.rs
+++ vrf/src/lib.rs
@@ -1,8 +1,9 @@
 use curve25519_dalek::constants::RISTRETTO_BASEPOINT_TABLE;
 use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 use curve25519_dalek::scalar::Scalar;
+use digest::generic_array::typenum::U64;
+use digest::Digest;
 use rand_core::OsRng;
-use sha3::Sha3_512;
 #[derive(Debug, PartialEq, Eq)]
 pub struct Proof {
     gamma: [u8; 32],
@@ -32,8 +33,11 @@ impl Proof {
 fn serialize_point(ristretto_point: RistrettoPoint) -> [u8; 32] {
     ristretto_point.compress().to_bytes()
 }
-pub fn prove(alpha: &[u8], secret: &Scalar) -> ([u8; 32], Proof) {
-    let h = RistrettoPoint::hash_from_bytes::<Sha3_512>(alpha);
+pub fn prove<D>(alpha: &[u8], secret: &Scalar) -> ([u8; 32], Proof)
+where
+    D: Digest<OutputSize = U64> + Default,
+{
+    let h = RistrettoPoint::hash_from_bytes::<D>(alpha);
     let p = &RISTRETTO_BASEPOINT_TABLE * secret;
     let gamma = h * secret;
     let k: Scalar = Scalar::random(&mut OsRng);
@@ -60,7 +64,10 @@ pub fn prove(alpha: &[u8], secret: &Scalar) -> ([u8; 32], Proof) {
         },
     )
 }
-pub fn verify(alpha: &[u8], beta: &[u8; 32], p: RistrettoPoint, pi: &Proof) -> bool {
+pub fn verify<D>(alpha: &[u8], beta: &[u8; 32], p: RistrettoPoint, pi: &Proof) -> bool
+where
+    D: Digest<OutputSize = U64> + Default,
+{
     let gamma = CompressedRistretto::from_slice(&pi.gamma).decompress();
     if gamma.is_none() {
         return false;
@@ -73,7 +80,7 @@ pub fn verify(alpha: &[u8], beta: &[u8; 32], p: RistrettoPoint, pi: &Proof) -> b
     let s = s.unwrap();
     let c_scalar = Scalar::from_bytes_mod_order(pi.c);
     let u = p * c_scalar + &RISTRETTO_BASEPOINT_TABLE * &s;
-    let h = RistrettoPoint::hash_from_bytes::<Sha3_512>(alpha);
+    let h = RistrettoPoint::hash_from_bytes::<D>(alpha);
     let v = gamma * c_scalar + h * s;
     beta == blake3::hash(&serialize_point(gamma)).as_bytes()
         && blake3::hash(
@@ -93,12 +100,13 @@ pub fn verify(alpha: &[u8], beta: &[u8; 32], p: RistrettoPoint, pi: &Proof) -> b
 mod tests {
     use super::*;
     use pea_key::Key;
+    use sha3::Sha3_512;
     #[test]
     fn test_proof() {
         let key = Key::generate();
         let alpha = [];
-        let (beta, pi) = prove(&alpha, &key.scalar);
-        assert!(verify(&alpha, &beta, key.ristretto_point(), &pi));
+        let (beta, pi) = prove::<Sha3_512>(&alpha, &key.scalar);
+        assert!(verify::<Sha3_512>(&alpha, &beta, key.ristretto_point(), &pi));
     }
     #[test]
     fn test_fake_proof() {
@@ -106,21 +114,21 @@ mod tests {
         let f_key = Key::generate();
         let alpha = [0];
         let f_alpha = [1];
-        let (beta, pi) = prove(&alpha, &key.scalar);
-        let (f_beta_0, f_pi) = prove(&alpha, &f_key.scalar);
+        let (beta, pi) = prove::<Sha3_512>(&alpha, &key.scalar);
+        let (f_beta_0, f_pi) = prove::<Sha3_512>(&alpha, &f_key.scalar);
         let mut f_beta_1 = beta.clone();
         f_beta_1[0] += 0x01;
-        assert!(!verify(&f_alpha, &beta, key.ristretto_point(), &pi));
-        assert!(!verify(&alpha, &beta, f_key.ristretto_point(), &pi));
-        assert!(!verify(&alpha, &f_beta_0, key.ristretto_point(), &pi));
-        assert!(!verify(&alpha, &f_beta_1, key.ristretto_point(), &pi));
-        assert!(!verify(&alpha, &beta, key.ristretto_point(), &f_pi));
+        assert!(!verify::<Sha3_512>(&f_alpha, &beta, key.ristretto_point(), &pi));
+        assert!(!verify::<Sha3_512>(&alpha, &beta, f_key.ristretto_point(), &pi));
+        assert!(!verify::<Sha3_512>(&alpha, &f_beta_0, key.ristretto_point(), &pi));
+        assert!(!verify::<Sha3_512>(&alpha, &f_beta_1, key.ristretto_point(), &pi));
+        assert!(!verify::<Sha3_512>(&alpha, &beta, key.ristretto_point(), &f_pi));
     }
     #[test]
     fn test_serialize() {
         let key = Key::generate();
         let alpha = [];
-        let (_, pi) = prove(&alpha, &key.scalar);
+        let (_, pi) = prove::<Sha3_512>(&alpha, &key.scalar);
         assert_eq!(pi, Proof::from_bytes(&pi.to_bytes()));
     }
 }

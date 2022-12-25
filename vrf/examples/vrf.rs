2f14d8a9583d66970d9bdeb61272622b4596b630
Lyserith <Lyserith@pm.me>
2022-12-25 17:51:18 +0100

    updates

--- vrf/examples/vrf.rs
+++ vrf/examples/vrf.rs
@@ -1,22 +1,22 @@
 use pea_key::Key;
 use pea_vrf::validate_key;
 use pea_vrf::{prove, verify};
-use sha2::Sha224;
-use sha2::Sha256;
-use sha2::Sha512;
+use sha3::Sha3_224;
+use sha3::Sha3_256;
+use sha3::Sha3_512;
 fn main() {
     let key = Key::generate();
     let secret = key.scalar;
     let public = key.compressed_ristretto().to_bytes();
     let alpha = [0; 32];
-    let proof = prove::<Sha512, Sha256>(&secret, &alpha);
-    let beta = proof.hash::<Sha224>();
+    let proof = prove::<Sha3_512, Sha3_256>(&secret, &alpha);
+    let beta = proof.hash::<Sha3_224>();
     let pi = proof.to_bytes();
     println!("public {}", hex::encode(public));
     println!("beta {}", hex::encode(beta));
     println!("pi {}", hex::encode(pi));
     println!(
         "verify {}",
-        validate_key(&public) && verify::<Sha512, Sha256, Sha224>(&public, &alpha, &pi, &beta)
+        validate_key(&public) && verify::<Sha3_512, Sha3_256, Sha3_224>(&public, &alpha, &pi, &beta)
     );
 }

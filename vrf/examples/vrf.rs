d7d152622df8c4c70736ade237e2b76f0d7a578e
Lyserith <Lyserith@pm.me>
2022-12-25 02:08:24 +0100

    updates

--- vrf/examples/vrf.rs
+++ vrf/examples/vrf.rs
@@ -5,19 +5,12 @@ use sha2::Sha256;
 use sha2::Sha512;
 fn main() {
     let key = Key::generate();
-    let mut alpha = [0; 32];
-    for _ in 0..3 {
-        for _ in 0..2 {
-            let proof = prove::<Sha512, Sha256>(&key.scalar, &alpha);
-            let beta = proof.hash::<Sha256>();
-            println!("{}", hex::encode(beta));
-        }
-        let proof = prove::<Sha512, Sha256>(&key.scalar, &alpha);
-        let beta = proof.hash::<Sha256>();
-        let pi = proof.to_bytes();
-        let public = key.compressed_ristretto().to_bytes();
-        println!("{}", hex::encode(beta));
-        println!("{}", validate_key(&public) && verify::<Sha512, Sha256>(&public, &alpha, &pi, &beta));
-        alpha = beta;
-    }
+    let secret = key.scalar;
+    let public = key.compressed_ristretto().to_bytes();
+    let alpha = [0; 32];
+    let proof = prove::<Sha512, Sha256>(&secret, &alpha);
+    let beta = proof.hash::<Sha256>();
+    let pi = proof.to_bytes();
+    println!("{}", hex::encode(beta));
+    println!("{}", validate_key(&public) && verify::<Sha512, Sha256>(&public, &alpha, &pi, &beta));
 }

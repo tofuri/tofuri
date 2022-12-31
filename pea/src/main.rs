2280830db3736bf08d8782a1795d85d9405d03b5
Lyserith <Lyserith@pm.me>
2022-12-31 05:32:51 +0000

    updates

--- pea/src/main.rs
+++ pea/src/main.rs
@@ -30,7 +30,7 @@ pub struct Args {
     #[clap(long, value_parser, default_value_t = false)]
     pub time_api: bool,
     /// Trust fork after blocks
-    #[clap(long, value_parser, default_value = "16")]
+    #[clap(long, value_parser, default_value = "2")]
     pub trust: usize,
     /// Pending blocks limit
     #[clap(long, value_parser, default_value = "256")]

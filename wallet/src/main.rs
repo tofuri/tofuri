68070b071dc69f1284c18a8a340aaf6eb9871cfe
zudo <44712315+zudo@users.noreply.github.com>
2023-02-21 18:52:49 +0100

    fix

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use pea_core::*;
-use pea_wallet::wallet::clear;
-use pea_wallet::wallet::press_any_key_to_continue;
-use pea_wallet::wallet::Wallet;
+use pea_wallet::clear;
+use pea_wallet::press_any_key_to_continue;
+use pea_wallet::Wallet;
 use pea_wallet::CARGO_PKG_NAME;
 use pea_wallet::CARGO_PKG_REPOSITORY;
 use pea_wallet::CARGO_PKG_VERSION;

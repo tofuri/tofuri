91844339403ffd5a2f1c8887df5f122f33de045f
zudo <44712315+zudo@users.noreply.github.com>
2023-05-04 00:50:33 +0200

    update

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -8,10 +8,10 @@ pub const CARGO_PKG_REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
 #[clap(version, about, long_about = None)]
 pub struct Args {
     /// API Endpoint
-    #[clap(long, value_parser, default_value = "[::]:2022")]
+    #[clap(long, env = "API", default_value = "[::]:2022")]
     pub api: SocketAddr,
 
     /// API Internal Endpoint
-    #[clap(long, value_parser, default_value = "[::]:2021")]
+    #[clap(long, env = "RPC", default_value = "[::]:2021")]
     pub rpc: SocketAddr,
 }

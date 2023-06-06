9dc6ba3c6fe5908c0b40389be0181bdd966383da
zudo <44712315+zudo@users.noreply.github.com>
2023-06-06 11:55:31 +0200

    ab

---
+++ transaction/src/b.rs
@@ -0,0 +1,99 @@
+use crate::Error;
+use crate::Transaction;
+use crate::TransactionA;
+use serde::Deserialize;
+use serde::Serialize;
+use serde_big_array::BigArray;
+use std::fmt;
+use tofuri_address::address;
+use tofuri_core::*;
+use tofuri_key::Key;
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub struct TransactionB {
+    pub output_address: AddressBytes,
+    pub amount: AmountBytes,
+    pub fee: AmountBytes,
+    pub timestamp: u32,
+    #[serde(with = "BigArray")]
+    pub signature: SignatureBytes,
+}
+impl TransactionB {
+    pub fn a(&self, input_address: Option<AddressBytes>) -> Result<TransactionA, Error> {
+        let input_address = input_address.unwrap_or(self.input_address()?);
+        let transaction_a = TransactionA {
+            output_address: self.output_address,
+            amount: tofuri_int::from_be_slice(&self.amount),
+            fee: tofuri_int::from_be_slice(&self.fee),
+            timestamp: self.timestamp,
+            signature: self.signature,
+            input_address,
+            hash: self.hash(),
+        };
+        Ok(transaction_a)
+    }
+    pub fn hash(&self) -> Hash {
+        crate::hash(self)
+    }
+    pub fn input_address(&self) -> Result<AddressBytes, Error> {
+        Ok(Key::address(&self.input_public_key()?))
+    }
+    pub fn input_public_key(&self) -> Result<PublicKeyBytes, Error> {
+        Key::recover(&self.hash(), &self.signature).map_err(Error::Key)
+    }
+}
+impl Transaction for TransactionB {
+    fn get_output_address(&self) -> &AddressBytes {
+        &self.output_address
+    }
+    fn get_timestamp(&self) -> u32 {
+        self.timestamp
+    }
+    fn get_amount_bytes(&self) -> AmountBytes {
+        self.amount
+    }
+    fn get_fee_bytes(&self) -> AmountBytes {
+        self.fee
+    }
+    fn hash(&self) -> Hash {
+        crate::hash(self)
+    }
+    fn hash_input(&self) -> [u8; 32] {
+        crate::hash_input(self)
+    }
+}
+impl Default for TransactionB {
+    fn default() -> TransactionB {
+        TransactionB {
+            output_address: [0; 20],
+            amount: [0; AMOUNT_BYTES],
+            fee: [0; AMOUNT_BYTES],
+            timestamp: 0,
+            signature: [0; 64],
+        }
+    }
+}
+impl fmt::Debug for TransactionB {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("TransactionB")
+            .field("output_address", &address::encode(&self.output_address))
+            .field("amount", &hex::encode(self.amount))
+            .field("fee", &hex::encode(self.fee))
+            .field("timestamp", &self.timestamp.to_string())
+            .field("signature", &hex::encode(self.signature))
+            .finish()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_hash() {
+        assert_eq!(
+            TransactionB::default().hash(),
+            [
+                102, 104, 122, 173, 248, 98, 189, 119, 108, 143, 193, 139, 142, 159, 142, 32, 8,
+                151, 20, 133, 110, 226, 51, 179, 144, 42, 89, 29, 13, 95, 41, 37
+            ]
+        );
+    }
+}
--- transaction/src/lib.rs
+++ transaction/src/lib.rs
@@ -1,12 +1,10 @@
-use serde::Deserialize;
-use serde::Serialize;
-use serde_big_array::BigArray;
+mod a;
+mod b;
+pub use a::TransactionA;
+pub use b::TransactionB;
 use sha2::Digest;
 use sha2::Sha256;
-use std::fmt;
-use tofuri_address::address;
 use tofuri_core::*;
-use tofuri_key::Key;
 #[derive(Debug)]
 pub enum Error {
     Key(tofuri_key::Error),
@@ -19,197 +17,16 @@ pub trait Transaction {
     fn hash(&self) -> Hash;
     fn hash_input(&self) -> [u8; 32];
 }
-impl Transaction for TransactionA {
-    fn get_output_address(&self) -> &AddressBytes {
-        &self.output_address
-    }
-    fn get_timestamp(&self) -> u32 {
-        self.timestamp
-    }
-    fn get_amount_bytes(&self) -> AmountBytes {
-        tofuri_int::to_be_bytes(self.amount)
-    }
-    fn get_fee_bytes(&self) -> AmountBytes {
-        tofuri_int::to_be_bytes(self.fee)
-    }
-    fn hash(&self) -> Hash {
-        hash(self)
-    }
-    fn hash_input(&self) -> [u8; 32] {
-        hash_input(self)
-    }
-}
-impl Transaction for TransactionB {
-    fn get_output_address(&self) -> &AddressBytes {
-        &self.output_address
-    }
-    fn get_timestamp(&self) -> u32 {
-        self.timestamp
-    }
-    fn get_amount_bytes(&self) -> AmountBytes {
-        self.amount
-    }
-    fn get_fee_bytes(&self) -> AmountBytes {
-        self.fee
-    }
-    fn hash(&self) -> Hash {
-        hash(self)
-    }
-    fn hash_input(&self) -> [u8; 32] {
-        hash_input(self)
-    }
-}
-#[derive(Clone, PartialEq, Serialize, Deserialize)]
-pub struct TransactionA {
-    pub input_address: AddressBytes,
-    pub output_address: AddressBytes,
-    pub amount: u128,
-    pub fee: u128,
-    pub timestamp: u32,
-    pub hash: Hash,
-    #[serde(with = "BigArray")]
-    pub signature: SignatureBytes,
-}
-impl fmt::Debug for TransactionA {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("TransactionA")
-            .field("input_address", &address::encode(&self.input_address))
-            .field("output_address", &address::encode(&self.output_address))
-            .field("amount", &tofuri_int::to_string(self.amount))
-            .field("fee", &tofuri_int::to_string(self.fee))
-            .field("timestamp", &self.timestamp.to_string())
-            .field("hash", &hex::encode(self.hash))
-            .field("signature", &hex::encode(self.signature))
-            .finish()
-    }
-}
-#[derive(Clone, PartialEq, Serialize, Deserialize)]
-pub struct TransactionB {
-    pub output_address: AddressBytes,
-    pub amount: AmountBytes,
-    pub fee: AmountBytes,
-    pub timestamp: u32,
-    #[serde(with = "BigArray")]
-    pub signature: SignatureBytes,
-}
-impl fmt::Debug for TransactionB {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("TransactionB")
-            .field("output_address", &address::encode(&self.output_address))
-            .field("amount", &hex::encode(self.amount))
-            .field("fee", &hex::encode(self.fee))
-            .field("timestamp", &self.timestamp.to_string())
-            .field("signature", &hex::encode(self.signature))
-            .finish()
-    }
-}
-impl TransactionA {
-    pub fn b(&self) -> TransactionB {
-        TransactionB {
-            output_address: self.output_address,
-            amount: tofuri_int::to_be_bytes(self.amount),
-            fee: tofuri_int::to_be_bytes(self.fee),
-            timestamp: self.timestamp,
-            signature: self.signature,
-        }
-    }
-    pub fn hash(&self) -> Hash {
-        hash(self)
-    }
-    pub fn sign(
-        output_address: AddressBytes,
-        amount: u128,
-        fee: u128,
-        timestamp: u32,
-        key: &Key,
-    ) -> Result<TransactionA, Error> {
-        let mut transaction_a = TransactionA {
-            input_address: [0; 20],
-            output_address,
-            amount: tofuri_int::floor(amount),
-            fee: tofuri_int::floor(fee),
-            timestamp,
-            hash: [0; 32],
-            signature: [0; 64],
-        };
-        transaction_a.hash = transaction_a.hash();
-        transaction_a.signature = key.sign(&transaction_a.hash).map_err(Error::Key)?;
-        transaction_a.input_address = key.address_bytes();
-        Ok(transaction_a)
-    }
-}
-impl TransactionB {
-    pub fn a(&self, input_address: Option<AddressBytes>) -> Result<TransactionA, Error> {
-        let input_address = input_address.unwrap_or(self.input_address()?);
-        let transaction_a = TransactionA {
-            output_address: self.output_address,
-            amount: tofuri_int::from_be_slice(&self.amount),
-            fee: tofuri_int::from_be_slice(&self.fee),
-            timestamp: self.timestamp,
-            signature: self.signature,
-            input_address,
-            hash: self.hash(),
-        };
-        Ok(transaction_a)
-    }
-    pub fn hash(&self) -> Hash {
-        hash(self)
-    }
-    fn input_address(&self) -> Result<AddressBytes, Error> {
-        Ok(Key::address(&self.input_public_key()?))
-    }
-    fn input_public_key(&self) -> Result<PublicKeyBytes, Error> {
-        Key::recover(&self.hash(), &self.signature).map_err(Error::Key)
-    }
-}
-fn hash<T: Transaction>(transaction: &T) -> Hash {
+pub fn hash<T: Transaction>(transaction: &T) -> Hash {
     let mut hasher = Sha256::new();
     hasher.update(transaction.hash_input());
     hasher.finalize().into()
 }
-fn hash_input<T: Transaction>(transaction: &T) -> [u8; 32] {
+pub fn hash_input<T: Transaction>(transaction: &T) -> [u8; 32] {
     let mut bytes = [0; 32];
     bytes[0..20].copy_from_slice(transaction.get_output_address());
     bytes[20..24].copy_from_slice(&transaction.get_timestamp().to_be_bytes());
     bytes[24..28].copy_from_slice(&transaction.get_amount_bytes());
     bytes[28..32].copy_from_slice(&transaction.get_fee_bytes());
     bytes
-}
-impl Default for TransactionA {
-    fn default() -> TransactionA {
-        TransactionA {
-            output_address: [0; 20],
-            amount: 0,
-            fee: 0,
-            timestamp: 0,
-            signature: [0; 64],
-            input_address: [0; 20],
-            hash: [0; 32],
-        }
-    }
-}
-impl Default for TransactionB {
-    fn default() -> TransactionB {
-        TransactionB {
-            output_address: [0; 20],
-            amount: [0; AMOUNT_BYTES],
-            fee: [0; AMOUNT_BYTES],
-            timestamp: 0,
-            signature: [0; 64],
-        }
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_hash() {
-        assert_eq!(
-            TransactionB::default().hash(),
-            [
-                102, 104, 122, 173, 248, 98, 189, 119, 108, 143, 193, 139, 142, 159, 142, 32, 8,
-                151, 20, 133, 110, 226, 51, 179, 144, 42, 89, 29, 13, 95, 41, 37
-            ]
-        );
-    }
 }
